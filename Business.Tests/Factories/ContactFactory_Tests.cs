1adc8ffa251dcd26a13ee13fd6cf348c1d4c5899
Max Natanaelsson <max.natanaelsson@outlook.com>
2024-12-09 18:27:12 +0100

    lagt till enhetstest som kontorllerar att contact skapas med korrekt data.

--- Business.Tests/Factories/ContactFactory_Tests.cs
+++ Business.Tests/Factories/ContactFactory_Tests.cs
@@ -45,4 +45,33 @@ public class ContactFactory_Tests
         Assert.Equal("0723370655", result.PhoneNumber);
         Assert.Equal("bondegatan 8", result.Address);
     }
+
+    [Fact]
+    public void Create_ShouldReturnContactWithCorrectData()
+    {
+        //arrange
+        var entity = new ContactEntity
+        {
+            Id = "1",
+            FirstName = "max",
+            LastName = "natanaelsson",
+            Email = "max.natanaelsson@outlook.com",
+            PhoneNumber = "0723370655",
+            Address = "bondegatan 8"
+        };
+
+        //act
+        var result = ContactFactory.Create(entity);
+
+        //assert
+        Assert.NotNull(result);
+        Assert.IsType<Contact>(result);
+        Assert.Equal(entity.Id, result.Id);
+        Assert.Equal(entity.FirstName, entity.FirstName);
+        Assert.Equal(entity.LastName, result.LastName);
+        Assert.Equal(entity.Email, result.Email);
+        Assert.Equal(entity.PhoneNumber, result.PhoneNumber);
+        Assert.Equal(entity.Address, result.Address);
+
+    }
 }
